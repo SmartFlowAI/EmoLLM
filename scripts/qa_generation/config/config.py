bb9915325574efce704310ad19c7bccf32cf9153
Max <max_zhang2020@outlook.com>
2024-03-07 23:59:10 +0800

    QA Generation - Update config.py

--- scripts/qa_generation/config/config.py
+++ scripts/qa_generation/config/config.py
@@ -3,22 +3,22 @@ import os
 """
 文件夹路径
 """
-cur_dir = os.path.dirname(os.path.abspath(__file__))                    # config
-base_dir = os.path.dirname(cur_dir)                                     # base
+cur_dir = os.path.dirname(os.path.abspath(__file__))                        # config
+base_dir = os.path.dirname(cur_dir)                                         # base
 
 # model
-model_dir = os.path.join(base_dir, 'model')                             # model
+model_dir = os.path.join(base_dir, 'model')                                 # model
 
 # data
-data_dir = os.path.join(base_dir, 'data')                               # data
-result_dir = os.path.join(data_dir, 'generated')                        # result
+data_dir = os.path.join(base_dir, 'data')                                   # data
+result_dir = os.path.join(data_dir, 'generated')                            # result
 
 # log
-log_dir = os.path.join(base_dir, 'log')                                 # log
-log_file_path = os.path.join(log_dir, 'log.log')                        # file
+log_dir = os.path.join(base_dir, 'log')                                     # log
+log_file_path = os.path.join(log_dir, 'log.log')                            # file
 
 # system prompt
-system_prompt_file_path = os.path.join(base_dir, 'system_prompt.md')    # system prompt
+system_prompt_file_path = os.path.join(base_dir, 'system_prompt_v2.md')     # system prompt
 
 
 """
