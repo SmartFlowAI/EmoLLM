d25a304c4d3990bc6d53c95a8f2dfef233321d99
HongCheng <kwchenghong@gmail.com>
2024-03-21 16:06:41 +0900

    Update process_single_turn_conversation_construction.py

--- datasets/processed/process_single_turn_conversation_construction.py
+++ datasets/processed/process_single_turn_conversation_construction.py
@@ -6,10 +6,6 @@ file_name = 'single_turn_dataset_2.json'
 with open(f'/root/StableCascade/emollm2/EmoLLM/datasets/{file_name}', 'rt', encoding='utf-8') as file:
     format1_data = json.load(file)
 
-# n = 0
-# for i in data:
-#     i['conversation'][0]['system'] = "你是心理健康助手EmoLLM，由EmoLLM团队打造。你旨在通过专业心理咨询，协助来访者完成心理诊断。请充分利用专业心理学知识与咨询技术，一步步帮助来访者解决心理问题。"
-
 system = "你是心理健康助手EmoLLM，由EmoLLM团队打造。你旨在通过专业心理咨询，协助来访者完成心理诊断。请充分利用专业心理学知识与咨询技术，一步步帮助来访者解决心理问题。"
 
 # 转换为格式2的数据
