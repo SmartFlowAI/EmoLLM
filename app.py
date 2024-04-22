5d0f60478b7b16d6087e8ea8deba16b94a99fbb5
HongCheng <kwchenghong@gmail.com>
2024-04-22 09:04:11 +0900

    update download_model

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ elif model == "EmoLLM_Model":
     os.system('streamlit run web_internlm2.py --server.address=0.0.0.0 --server.port 7860')
 elif model == "Llama3_Model":
     os.system("python download_model.py chg0901/EmoLLM-Llama3-8B-Instruct2.0")
+    # os.system('streamlit run web_demo-Llama3_online.py --server.address=0.0.0.0 --server.port 7860')
     os.system('streamlit run web_demo-Llama3_online.py --server.address=0.0.0.0 --server.port 7860')
 else:
     print("Please select one model")
