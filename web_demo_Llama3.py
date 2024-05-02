8d6571be46725c005aef432249ae540541959f6e
HongCheng <kwchenghong@gmail.com>
2024-05-03 03:02:14 +0900

    Update web_demo_Llama3.py

--- web_demo_Llama3.py
+++ web_demo_Llama3.py
@@ -214,7 +214,7 @@ def combine_history(prompt):
 
 def main():
 
-    torch.cuda.empty_cache()
+    # torch.cuda.empty_cache()
     print("load model begin.")
     model, tokenizer = load_model()
     print("load model end.")
@@ -222,7 +222,7 @@ def main():
     user_avator = "assets/user.png"
     robot_avator = "assets/EmoLLM.png"
 
-    st.title("EmoLLM Llama3心理咨询室V2.0")
+    st.title("EmoLLM Llama3心理咨询室V3.0")
 
     generation_config = prepare_generation_config()
 
@@ -268,5 +268,5 @@ def main():
         torch.cuda.empty_cache()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
