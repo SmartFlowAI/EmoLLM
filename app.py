b83270bb13ca6db2e83c80b88002e36c9f25ba52
HongCheng <kwchenghong@gmail.com>
2024-05-03 01:02:55 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 import os
 
-#model = "EmoLLM_aiwei"
-# model = "EmoLLM_Model"
-model = "Llama3_Model"
+os.system('streamlit run web_demo_Llama3.py --server.address=0.0.0.0 --server.port 7860')
 
-if model == "EmoLLM_aiwei":
-    os.system("python download_model.py ajupyter/EmoLLM_aiwei")
-    os.system('streamlit run web_demo-aiwei.py --server.address=0.0.0.0 --server.port 7860')
-elif model == "EmoLLM_Model":
-    os.system("python download_model.py jujimeizuo/EmoLLM_Model")
-    os.system('streamlit run web_internlm2.py --server.address=0.0.0.0 --server.port 7860')
-elif model == "Llama3_Model":
-    os.system('streamlit run web_demo_Llama3.py --server.address=0.0.0.0 --server.port 7968')
-else:
-    print("Please select one model")
+# #model = "EmoLLM_aiwei"
+# # model = "EmoLLM_Model"
+# model = "Llama3_Model"
+
+# if model == "EmoLLM_aiwei":
+#     os.system("python download_model.py ajupyter/EmoLLM_aiwei")
+#     os.system('streamlit run web_demo-aiwei.py --server.address=0.0.0.0 --server.port 7860')
+# elif model == "EmoLLM_Model":
+#     os.system("python download_model.py jujimeizuo/EmoLLM_Model")
+#     os.system('streamlit run web_internlm2.py --server.address=0.0.0.0 --server.port 7860')
+# elif model == "Llama3_Model":
+#     os.system('streamlit run web_demo_Llama3.py --server.address=0.0.0.0 --server.port 7860')
+# else:
+#     print("Please select one model")
